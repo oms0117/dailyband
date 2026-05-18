0b080abb50406e5a015abee2cdcdda5e0dc58d69
oms0117 <dbalstmd8107@gmail.com>
2026-05-18 22:20:55 +0900

    Update sever.js

--- sever.js
+++ sever.js
@@ -3,42 +3,35 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
- });
+});
 
-// 웹브라우저에 띄울 HTML 파일들이 들어있는 public 폴더 지정
+// public 폴더 연결
 app.use(express.static(__dirname + '/public'));
 
-// ⭐ [중요] 24시간 잠들지 않게 봇이 깨워줄 때 응답할 가벼운 통로 생성
+// 24시간 안 잠들게 하는 체크용 경로
 app.get('/live-check', (req, res) => {
     res.send('SERVER_IS_ALIVE');
 });
 
+// 소켓 연결 설정
 io.on('connection', (socket) => {
-    console.log('단원 접속 (ID):', socket.id);
-
     socket.on('join-voicetalk', (roomId) => {
         socket.join(roomId);
         socket.to(roomId).emit('user-connected', socket.id);
     });
-
     socket.on('offer', (data) => {
         socket.to(data.roomId).emit('offer', { sdp: data.sdp, sender: socket.id });
     });
-
     socket.on('answer', (data) => {
         socket.to(data.roomId).emit('answer', { sdp: data.sdp, sender: socket.id });
     });
-
     socket.on('ice-candidate', (data) => {
         socket.to(data.roomId).emit('ice-candidate', { candidate: data.candidate, sender: socket.id });
     });
-
-    socket.on('disconnect', () => {
-        console.log('단원 접속 종료 (ID):', socket.id);
-    });
 });
 
+// 포트 설정 및 서버 실행
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`📞 보이스톡 서버 가동 포트: ${PORT}`);
+    console.log(`SERVER_IS_RUNNING_ON_PORT_${PORT}`);
 });
